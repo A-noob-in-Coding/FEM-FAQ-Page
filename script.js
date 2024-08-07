56889eb568da762cbbe7d27a6d837039ecd48be0
A_noob_in_coding <naeemabdulrehman77@gmail.com>
2024-08-07 16:07:28 +0500

    added transition to the description

--- script.js
+++ script.js
@@ -6,58 +6,95 @@ let btn_1 = document.getElementById("btn-1")
 let btn_2 = document.getElementById("btn-2")
 let btn_3 = document.getElementById("btn-3")
 let btn_4 = document.getElementById("btn-4")
+let first_title = document.getElementById("first-title")
+let second_title = document.getElementById("second-title")
+let third_title = document.getElementById("third-title")
+let forth_title = document.getElementById("forth-title")
+//functions
 
-btn_1.addEventListener("click",()=>{
-    if(first_des.style.display == "block"){
-        first_des.style.display = "none"
+function first_btn_handler(){
+    if(first_des.classList.contains("visible")){
+        first_des.classList.remove("visible")
         document.getElementById("first-plus").style.display = "block"   
         document.getElementById("first-minus").style.display = "none"   
     }
     else{
         document.getElementById("first-plus").style.display = "none"   
         document.getElementById("first-minus").style.display = "block"   
-        first_des.style.display = "block"
+        first_des.classList.add("visible")
     }
-})
+}
 
-btn_2.addEventListener("click",()=>{
-    if(second_des.style.display == "block"){
-        document.getElementById("second-plus").style.display = "block"   
-        document.getElementById("second-minus").style.display = "none"   
-        second_des.style.display = "none"
-        
-    }
-    else{
-        document.getElementById("second-plus").style.display = "none"   
-        document.getElementById("second-minus").style.display = "block"   
-        second_des.style.display = "block"
-    }
-})
 
-btn_3.addEventListener("click",()=>{
-    if(third_des.style.display == "block"){
-        third_des.style.display = "none"
+function second_btn_handler(){
+    if (second_des.classList.contains("visible")) {
+        document.getElementById("second-plus").style.display = "block";
+        document.getElementById("second-minus").style.display = "none";
+        second_des.classList.remove("visible");
+    } else {
+        console.log("to");
+        document.getElementById("second-plus").style.display = "none";
+        document.getElementById("second-minus").style.display = "block";
+        second_des.classList.add("visible");
+    }
+}
+
+function third_btn_handler(){
+    if(third_des.classList.contains("visible")){
         document.getElementById("third-plus").style.display = "block"   
         document.getElementById("third-minus").style.display = "none"   
         
+        third_des.classList.remove("visible")
     }
     else{
         document.getElementById("third-plus").style.display = "none"   
         document.getElementById("third-minus").style.display = "block"   
-        third_des.style.display = "block"
+        third_des.classList.add("visible")
     }
-})
+}
 
-btn_4.addEventListener("click",()=>{
-    if(forth_des.style.display == "block"){
+function forth_btn_handler(){
+    if(forth_des.classList.contains("visible")){
         document.getElementById("forth-plus").style.display = "block"   
         document.getElementById("forth-minus").style.display = "none"   
-        forth_des.style.display = "none"
+        forth_des.classList.remove("visible")
         
     }
     else{
-        forth_des.style.display = "block"
+        forth_des.classList.add("visible")
         document.getElementById("forth-plus").style.display = "none"   
         document.getElementById("forth-minus").style.display = "block"   
     }
+}
+
+btn_1.addEventListener("click",()=>{
+    first_btn_handler()    
+})
+
+btn_2.addEventListener("click", () => {
+    second_btn_handler()    
+});
+
+btn_3.addEventListener("click",()=>{
+    third_btn_handler()
+})
+
+btn_4.addEventListener("click",()=>{
+    forth_btn_handler()
+})
+
+first_title.addEventListener("click",()=>{
+    first_btn_handler()
+})
+
+second_title.addEventListener("click",()=>{
+    second_btn_handler()
+})
+
+third_title.addEventListener("click",()=>{
+    third_btn_handler()
+})
+
+forth_title.addEventListener("click",()=>{
+    forth_btn_handler()
 })
